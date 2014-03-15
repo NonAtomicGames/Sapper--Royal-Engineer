aedcb46835503bc60bf7d9d54287c9a780cc8d1e
AndrewShmig <andrewshmig@gmail.com>
2014-03-15 15:04:51 +0400

    Initial Commit

---
+++ Miner/BGViewController.h
@@ -0,0 +1,13 @@
+//
+//  BGViewController.h
+//  Miner
+//
+//  Created by AndrewShmig on 3/15/14.
+//  Copyright (c) 2014 Bleeding Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGViewController : UIViewController
+
+@end
