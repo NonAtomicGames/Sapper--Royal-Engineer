0f29fda365349b9d1aca78c6babf0cbd104580b3
AndrewShmig <andrewshmig@gmail.com>
2014-05-17 20:45:52 +0400

    +добавлено выравниение надписи с финальным счетом на плашке

--- Miner/Classes/BGMinerField/BGMinerField.m
+++ Miner/Classes/BGMinerField/BGMinerField.m
@@ -3,7 +3,7 @@
 //  Miner
 //
 //  Created by AndrewShmig on 3/15/14.
-//  Copyright (c) 2014 Bleeding Games. All rights reserved.
+//  Copyright (c) 2014 Non Atomic Games. All rights reserved.
 //
 
 #import "BGMinerField.h"
