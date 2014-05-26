677657e521a5463b40d3dcdd131972b80634959f
AndrewShmig <andrewshmig@gmail.com>
2014-05-26 11:43:18 +0400

    +добавлен предзагрузчик ресурсов

---
+++ Miner/Classes/NAGResourcePreloader/NAGResourcePreloader.h
@@ -0,0 +1,13 @@
+//
+//  NAGResourcePreloader.h
+//  Miner
+//
+//  Created by AndrewShmig on 5/26/14.
+//  Copyright (c) 2014 Non Atomic Games. All rights reserved.
+//
+
+
+
+@interface NAGResourcePreloader : NSObject
+
+@end
